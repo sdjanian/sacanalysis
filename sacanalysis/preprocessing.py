Added some saccade analysis unittesting, changed residuals from sum to mean, fixed z-score saccade calculation and committed some minor changes

sacanalysis/preprocessing.py
@@ -1,6 +1,7 @@
 # Python libraries
 import pandas as pd
 import numpy as np
+import copy
 from scipy import stats
 from .utility import getTimeinSec
 
@@ -56,10 +57,10 @@ class Preproccesing:
         self.__saccades = saccades
         self.__one_sample_duration = getTimeinSec(time_unit)/Hz 
             
-    def __preprocessSaccades(self):
+    def __preprocessSaccades(self,ratio_threshold=5):
         self.__RemoveSaccadesWithUnstableTimestamps()
         self.__GetHorVerRatio()
-        self.__ChooseOnlyHoriztonalOrVerticalSaccades()
+        self.__ChooseOnlyHoriztonalOrVerticalSaccades(ratio_threshold)
         self.__NormalizePositionalAndVelocitySignal()
         self.__TransformSaccadesToUpwardsDirection()
         self.__ZScoreTransformPosition()
@@ -84,7 +85,7 @@ class Preproccesing:
         x_y_ratios_array =[]
         for ratio in x_y_ratios:
             x_y_ratios_array.extend(ratio)     
-        self.__stable_timestamps["x_y_ratio"] = x_y_ratios_array
+        self.__stable_timestamps.loc[:,"x_y_ratio"] = x_y_ratios_array
                          
 
 
@@ -94,7 +95,10 @@ class Preproccesing:
         Filter saccades based on threshold. With a ratio of above 5 the saccedes are mainly horizontal, 
         meaning that the horizontal position is 5 times larger then the vertical.
         '''
-        self.__saccadesToProcces = self.__stable_timestamps[self.__stable_timestamps["x_y_ratio"]>ratio_threshold]
+        if ratio_threshold == None:
+            self.__saccadesToProcces = self.__stable_timestamps
+        else:
+            self.__saccadesToProcces = self.__stable_timestamps[self.__stable_timestamps["x_y_ratio"]>ratio_threshold]
         
     def __NormalizePositionalAndVelocitySignal(self)-> None:
         """
@@ -102,14 +106,14 @@ class Preproccesing:
         Adds the normalized position to dataframe as "x_norm" and "y_norm" columns respectivly.
         """
         # Normalise the horizontal position
-        self.__saccadesToProcces["x_norm"] = self.__saccadesToProcces.groupby("unique_saccade_number")["x"].transform(
+        self.__saccadesToProcces.loc[:,"x_norm"] = self.__saccadesToProcces.groupby("unique_saccade_number")["x"].transform(
                 lambda x: x-x.mean() 
                 )
         # Normalise the vertical position
-        self.__saccadesToProcces["y_norm"] = self.__saccadesToProcces.groupby("unique_saccade_number")["y"].transform(
+        self.__saccadesToProcces.loc[:,"y_norm"] = self.__saccadesToProcces.groupby("unique_saccade_number")["y"].transform(
                 lambda y: y-y.mean() 
                 )
-        self.__saccadesToProcces["velocity_norm"] = self.__saccadesToProcces.groupby("unique_saccade_number")["velocity"].transform(
+        self.__saccadesToProcces.loc[:,"velocity_norm"] = self.__saccadesToProcces.groupby("unique_saccade_number")["velocity"].transform(
         lambda x: x-x.mean()
         )
         
@@ -118,7 +122,7 @@ class Preproccesing:
         Convert the saccades to all point upwards based on the mean normalized horitzontal position.
         Adds the upwards pointing saccades as "x_norm_up".
         """
-        self.__saccadesToProcces["x_norm_up"] = self.__saccadesToProcces.groupby("unique_saccade_number")["x_norm"].apply(
+        self.__saccadesToProcces.loc[:,"x_norm_up"] = self.__saccadesToProcces.groupby("unique_saccade_number")["x_norm"].apply(
                 lambda x: x*-1 if x.iloc[0]-x.iloc[-1] > 0 else x
                 ) 
         
@@ -127,21 +131,28 @@ class Preproccesing:
         Z score transforms horizontal position of the upwards pointing saccades.
         """
         # Z score transform the horizontal position
-        self.__saccadesToProcces["x_z_trans"] = self.__saccadesToProcces.groupby("unique_saccade_number")["x_norm_up"].transform(
+        self.__saccadesToProcces.loc[:,"x_z_trans"] = self.__saccadesToProcces.groupby("unique_saccade_number")["x_norm_up"].transform(
                 lambda x: stats.zscore(x)
                 )        
     def __CalcuateAvergeSaccade(self) -> pd.DataFrame:
-        # Average saccade in z transformed domain
-        average_saccade_z_score = self.__saccadesToProcces.groupby("norm_time")["x_z_trans"].apply(
-                lambda x: np.mean(x)
-                )
         
         # Average saccade in normalized degrees
         average_saccade_norm_deg = self.__saccadesToProcces.groupby("norm_time")["x_norm_up"].apply(
                 lambda x: np.mean(x)
                 )
         
-        # Average saccade velocity (using velocity)
+        """ Wrong way to calculate the z-scored average saccade
+        # Average saccade in z transformed domain
+        average_saccade_z_score = self.__saccadesToProcces.groupby("norm_time")["x_z_trans"].apply(
+                lambda x: np.mean(x)
+                )
+        """
+        
+        # Average saccade in z transformed domain
+        average_saccade_z_score = pd.Series(data=stats.zscore(average_saccade_norm_deg),
+                                            name = "x_z_trans",
+                                            index=average_saccade_norm_deg.index)
+        
         average_saccade_velocity = self.__saccadesToProcces.groupby("norm_time")["velocity_norm"].apply(
                 lambda x: np.mean(x)
                 )
@@ -162,7 +173,7 @@ class Preproccesing:
             return np.hstack(vector).squeeze()
 
         wide_form = self.__saccadesToProcces.pivot(index="unique_saccade_number",columns = "norm_time",values = "velocity")
-        wide_form["vector"] = wide_form.apply(lambda x: vectorizeHelperFunction(x) if x.index.name!="unique_saccade_number" else None,axis=1)
+        wide_form.loc[:,"vector"] = wide_form.apply(lambda x: vectorizeHelperFunction(x) if x.index.name!="unique_saccade_number" else None,axis=1)
         wide_form=wide_form.reset_index()
         wide_form=wide_form.rename(columns={"index":"unique_saccade_number"})
         #normalized_saccades["vector"] = normalized_saccades.groupby("unique_saccade_number")["x_norm_up"].transform(
@@ -190,11 +201,11 @@ class Preproccesing:
         """
         return self.__GenerateHistogramVector()
     
-    def GetPreprocessedSaccades(self) -> pd.DataFrame:
+    def GetPreprocessedSaccades(self,ratio_threshold = 5) -> pd.DataFrame:
         """
         Runs the preprocessing and returns the saccades in a df where each row is a time stamp and the columns are the different processes.
         """
-        self.__preprocessSaccades()
+        self.__preprocessSaccades(ratio_threshold=ratio_threshold)
         return self.__saccadesToProcces
 
     @staticmethod

sacanalysis/saccade_analysis/saccade_analysis.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 from scipy.stats import entropy
 from scipy.stats import kurtosis, skew
+
 from .get_dip import getDip
 from .rjmcmc import rjmcmc_output
+from ..utility.error_handling import xyTimeVelocityCheck, sourceCheck
 
 class SaccadeAnalysis:
     """
@@ -53,23 +55,25 @@ class SaccadeAnalysis:
     """    
 
     def __init__(self,saccades:pd.DataFrame, average_saccade:pd.DataFrame,histogram_velocity_vector:pd.DataFrame):
+        xyTimeVelocityCheck(saccades)
+        saccades = sourceCheck(saccades)
         self.__saccades = saccades
         self.__average_saccade = average_saccade
         self.__histogram_velocity_vector = histogram_velocity_vector
         self.__one_sample_duration = 1000/250 
     
     
-    def __CalcuateScores(self):
+    def __CalcuateScores(self,windows_size: int=3):
         
         # Create residual dataframe
         self.__scores = self.__saccades.groupby("unique_saccade_number").apply(
-                lambda x: x[["unique_saccade_number","source","subject","stimuli","start_time","end_time"]].iloc[0]
+                lambda x: x[["unique_saccade_number","source","start_time","end_time"]].iloc[0]
                 )
 
         self.__ResidualZScoreSaccade()
         self.__ResidualUpwardsSaccade()
         self.__ResidualVelocity()
-        self.__FlatnessScore()
+        self.__FlatnessScore(windows_size=windows_size)
         
         #Vector analyses
         self.__AddHistogramVectorToScore()       
@@ -89,7 +93,7 @@ class SaccadeAnalysis:
                 )   
         
         self.__scores["residual_sum_x_z_trans"] = self.__saccades.groupby("unique_saccade_number")["residual_x_z_trans"].apply(
-                lambda x:np.sum(x)
+                lambda x:np.mean(x)
                 )
     def __ResidualUpwardsSaccade(self) -> None:
         """Calculate the residual for each saccade from the average saccade. The norm is applied to ensure all values are positive."""
@@ -98,7 +102,7 @@ class SaccadeAnalysis:
                 ) 
         # Calculate the sum of the residuals
         self.__scores["residual_sum_x_norm_up"] = self.__saccades.groupby("unique_saccade_number")["residual_x_norm_up"].apply(
-                lambda x:np.sum(x)
+                lambda x:np.mean(x)
                 )        
     
     def __ResidualVelocity(self) -> None:
@@ -111,7 +115,7 @@ class SaccadeAnalysis:
                 )
         # Calculate the sum of the residuals
         self.__scores["residual_sum_velocity"] = self.__saccades.groupby("unique_saccade_number")["residual_velocity"].apply(
-                    lambda x:np.sum(x)
+                    lambda x:np.mean(x)
                     )
 
     def __FlatnessScore(self,windows_size:int=3) -> None:
@@ -184,7 +188,7 @@ class SaccadeAnalysis:
                 self.__scores["ranked_"+residual_col] = normalizeRanks(self.__scores.index)
             self.__scores["ranked_"+residual_col][self.__scores[residual_col].isna()==True]=np.nan     
             
-    def GetScores(self) -> pd.DataFrame:
+    def GetScores(self,windows_size: int=3) -> pd.DataFrame:
         """
             Returns a dataframe with all the preproccesed data added as
             ['residual_sum_x_z_trans'       : Residual sum of the horizontal signal from average saccade in z-score transformation , 
@@ -204,7 +208,7 @@ class SaccadeAnalysis:
                                               for https://pdfs.semanticscholar.org/29a4/f9cd5faa12593ac13d0349ba842d48eb792a.pdf
             and each row is a timestamp sample of a saccade.
         """
-        self.__CalcuateScores()
+        self.__CalcuateScores(windows_size)
         return self.__scores
         
     

sacanalysis/test/single_recording_analysis.py
@@ -3,33 +3,36 @@ from scipy.io import arff
 import numpy as np
 import pandas as pd
 if __name__=="__main__":
-    data, meta = arff.loadarff("AAF_beach.arff")
-    df_single_rec = pd.DataFrame(data)  
-    df_single_rec[df_single_rec["confidence"]==0] = np.nan
-
-    df_single_rec = df_single_rec[["time","x","y","handlabeller_final"]]
-    df_single_rec = df_single_rec.rename(columns = {"handlabeller_final":"labels"})
+    #data, meta = arff.loadarff("AAF_beach.arff")
+    #df = pd.DataFrame(data)  
     
-    df_single_rec = GetVelocity(df_single_rec, "time")
-    events = CalculateEventDurationClass.calculateBasicEventStatistics(df_single_rec,label_col="labels")
+    load_gazecom = Load_gazecom("C:/sacanalysis/gazecom_small/all_features")
+    df = load_gazecom.load_all_data(unit_for_time="milli")
+    df[df["confidence"]==0] = np.nan
+
+    df = df[["time","x","y","handlabeller_final","source","speed_1"]]
+    df = df.rename(columns = {"handlabeller_final":"labels","speed_1":"velocity"})
+    
+    #df = GetVelocity(df, "time",time_unit="milli")
+    events = CalculateEventDurationClass.calculateBasicEventStatistics(df,label_col="labels")
     events = events.dropna()
     duration = events["event_duration"].value_counts().index.values[0] # most frequent duration
     
     sub_event_df = events[events["event_duration"]==duration]
-    extracted_saccades = CalculateEventDurationClass.extractEventsToDdf(df_single_rec,
+    extracted_saccades = CalculateEventDurationClass.extractEventsToDdf(df,
                                                                         sub_event_df,
                                                                         event_label = Load_gazecom.class_event_gazecom["Saccade"],
-                                                                        label_col = "labels")
+                                                                        label_col = "labels",verbose=0)
     #extracted_saccades = extracted_saccades.groupby(by=["unique_saccade_number"]).apply(lambda x: x.dropna()) # check for nans
-
-    preproces = Preproccesing(extracted_saccades,Hz = 250, time_unit="micro")
     
-    #extracted_saccades["x_norm"] = extracted_saccades.groupby("unique_saccade_number")["x"].transform(
-    #    lambda x: x-x.mean() 
-    #    )
-    processed_saccades = preproces.GetPreprocessedSaccades()
-    average_saccade = preproces.GetAverageSaccade()
-    histogram_vector = preproces.GetHistogramVectors()    
+    preproces = Preproccesing(extracted_saccades,Hz = 250, time_unit="milli")
+    
+    
+    #processed_saccades = preproces.GetPreprocessedSaccades(None)
+    #average_saccade = preproces.GetAverageSaccade()
+    #histogram_vector = preproces.GetHistogramVectors()   
+    #_SaccadeAnalysis = SaccadeAnalysis(processed_saccades,average_saccade,histogram_vector)
+    #scores = _SaccadeAnalysis.GetScores()
     #preprocessed_saccades = preproces.GetPreprocessedSaccades()
     """
     sub_event_df = event_df[event_df["event_duration"]==duration]

sacanalysis/unittest/manual_preprocess_calculations.py
@@ -46,14 +46,13 @@ if __name__ == '__main__':
 
     mean_sac_x = [None]*6
     mean_sac_x_norm_up = [None]*6
-    mean_sac_z =[None]*6
     mean_sac_vel = [None]*6
     for i in np.arange(0,6): 
         mean_sac_x[i] = np.mean([x_1[i],x_2[i]])
         mean_sac_x_norm_up[i] = np.mean([x_1_norm_up[i],x_2_norm_up[i]])
-        mean_sac_z[i] = np.mean([z_score_1[i],z_score_2[i]])
         mean_sac_vel[i] = np.mean([velocity_1_norm[i],velocity_2_norm[i]])
-        
+    
+    mean_sac_z = stats.zscore(mean_sac_x_norm_up)
     df_mean_sac = pd.DataFrame({"x_z_trans":mean_sac_z,"x_norm_up":mean_sac_x_norm_up,"velocity_norm":mean_sac_vel})
     df_mean_sac.to_csv("20_ms_average_saccade_manual_calculation.csv")
     

sacanalysis/utility/feature_extraction.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+import tqdm
 from .time_units import getTimeinSec
 from .error_handling import sourceCheck, xyCheck
 
-def GetVelocity(eye_movement_signal:pd.DataFrame,timestamp_col:str,windowsWidth:int = 1, time_unit:str = "micro"):    
+def GetVelocity(eye_movement_signal:pd.DataFrame,timestamp_col:str,windowWidth:int = 1, time_unit:str = "micro"):    
     """   
     Description
     ---------- 
@@ -33,13 +34,45 @@ def GetVelocity(eye_movement_signal:pd.DataFrame,timestamp_col:str,windowsWidth:
     
 
     xyCheck(eye_movement_signal) # sanity check
-    eye_movement_signal = sourceCheck(eye_movement_signal)
-    x = eye_movement_signal["x"]
-    y = eye_movement_signal["y"]
-    velocity_list = np.zeros(len(eye_movement_signal))
-    windowWidth = 1
+    if timestamp_col != None:
+        None
+    else:
+        raise Exception("timestamp_col not defined")    
+        
+    if "source" in eye_movement_signal.columns:
+        print("Source on col")
+        eye_movement_signal = __CalculateVelocityOnMultipleRecordings(eye_movement_signal,timestamp_col,windowWidth, time_unit)
+        
+
+    else:                
+        eye_movement_signal = sourceCheck(eye_movement_signal)
+        x = eye_movement_signal["x"]
+        y = eye_movement_signal["y"]
+        timestamp = eye_movement_signal[timestamp_col]
+        #velocity_list = np.zeros(len(eye_movement_signal))
+        eye_movement_signal["velocity"] = __CalcateVelocity(x,y,timestamp,windowWidth,time_unit)
+
+    
+     #= velocity_list
+    return eye_movement_signal
+
+def __CalculateVelocityOnMultipleRecordings(eye_movement_signal:pd.DataFrame,timestamp_col:str,windowWidth:int = 1, time_unit:str = "micro"):
+    recording_list =[]
+    groups = [grp for di, grp in eye_movement_signal.groupby('source')] # Faster too loop through the list then get extract each subset during the loop.
+    for recording in tqdm.tqdm(groups,total=len(groups),position=1,leave=True,desc="Calculating velocity on multple recordings"):
+        x = recording["x"]
+        y = recording["y"]
+        timestamp = recording[timestamp_col]
+        #velocity_list = np.zeros(len(eye_movement_signal))
+        recording["velocity"] = __CalcateVelocity(x,y,timestamp,windowWidth,time_unit)
+        recording_list.append(recording)
+    eye_movement_signal = pd.concat(recording_list)
+    return eye_movement_signal
+
+def __CalcateVelocity(x,y,timestamp,windowWidth:int = 1, time_unit:str = "micro"):
     step = int(np.ceil(windowWidth/2))
-    data_size = len(eye_movement_signal)
+    data_size = len(x)
+    velocity_list = np.zeros(data_size)
     for i in range(data_size):
     
         # get initial interval
@@ -57,23 +90,19 @@ def GetVelocity(eye_movement_signal:pd.DataFrame,timestamp_col:str,windowsWidth:
         if (endPos > data_size):
             endPos = i
         
-        if (x[endPos] is np.nan or x[startPos] is np.nan or
-            y[endPos] is np.nan or y[startPos] is np.nan):
+        if (x.iloc[endPos] is np.nan or x.iloc[startPos] is np.nan or
+            y.iloc[endPos] is np.nan or y.iloc[startPos] is np.nan):
             velocity = np.nan
             continue
         
-        ampl = np.sqrt(((x[endPos]-x[startPos])**2+(y[endPos]-y[startPos])**2))
-        if timestamp_col != None:
-            delta_t =(eye_movement_signal[timestamp_col].iloc[endPos]-eye_movement_signal[timestamp_col].iloc[startPos])
-            time = __safe_div(delta_t,getTimeinSec(time_unit))   
-        else:
-            raise Exception("timestamp_col not defined")
+        ampl = np.sqrt(((x.iloc[endPos]-x.iloc[startPos])**2+(y.iloc[endPos]-y.iloc[startPos])**2))
+        delta_t =(timestamp.iloc[endPos]-timestamp.iloc[startPos])
+        time = __safe_div(delta_t,getTimeinSec(time_unit))   
+
              
         velocity = np.round(__safe_div(ampl,time),2)
         velocity_list[i] = velocity
-    
-    eye_movement_signal["velocity"] = velocity_list
-    return eye_movement_signal
+    return velocity_list
 
 def __safe_div(x,y):
     if y == 0:
@@ -81,28 +110,37 @@ def __safe_div(x,y):
     return x / y
 
 if __name__=="__main__":
-    """
-    df = pd.DataFrame({"x":[1,1,1,1,1,1,2,2,2,2,2,3,3,3,3,6,7,8,8,9,9,9,9,9,7,7,7,5,5,4,3,22,2],
-                       "y":[5,5,5,6,6,6,30,30,7,7,7,24,25,24,23,50,30,50,8,9,5,3,7,10,11,23,24,52,54,52,56,52,32]})
-    
-    test =shift(df["x"],4,cval=np.NaN)
 
-
-    x1 = 590.9
-    y1 = 5.2
-    
-    x2 = 590.6
-    y2 = 5.0
-    """
     from scipy.io import arff
-
-    data, meta = arff.loadarff(r"C:\GazeCom_Data\all_features\beach\AAF_beach.arff")
-    df_original = pd.DataFrame(data)   
-    #example = (np.sqrt((x2-x1)**2)+np.sqrt((y2-y1)**2))/(4000/1000000)
+    from pandas.testing import assert_series_equal
+    from sacanalysis import Load_gazecom
+    # check multple recordings
+    load_gazecom = Load_gazecom("C:/sacanalysis/gazecom_small/all_features")
+    df_multiple_rec = load_gazecom.load_all_data(unit_for_time="Milli")
+    list_of_rec = df_multiple_rec["source"].unique()
+    rec_1 = df_multiple_rec[df_multiple_rec["source"]==list_of_rec[0]].drop(columns=(["source"]))
+    rec_2 = df_multiple_rec[df_multiple_rec["source"]==list_of_rec[1]].drop(columns=(["source"]))
+    rec_3 = df_multiple_rec[df_multiple_rec["source"]==list_of_rec[2]].drop(columns=(["source"]))
+    
+    df_multiple_rec_vel = GetVelocity(df_multiple_rec,timestamp_col="time")
+    rec_1_vel = GetVelocity(rec_1,timestamp_col="time")
+    rec_2_vel = GetVelocity(rec_2,timestamp_col="time")
+    rec_3_vel = GetVelocity(rec_3,timestamp_col="time")  
+    assert_series_equal(df_multiple_rec_vel["velocity"][df_multiple_rec["source"]==list_of_rec[0]],
+                        rec_1_vel["velocity"],check_less_precise=True,check_names=False)
+    assert_series_equal(df_multiple_rec_vel["velocity"][df_multiple_rec["source"]==list_of_rec[1]],
+                        rec_2_vel["velocity"],check_less_precise=True,check_names=False)
+    assert_series_equal(df_multiple_rec_vel["velocity"][df_multiple_rec["source"]==list_of_rec[2]],
+                        rec_3_vel["velocity"],check_less_precise=True,check_names=False)
+    
+    subdf = df_multiple_rec_vel[df_multiple_rec["source"]==list_of_rec[1]]
+    
+    # Check single recording
+    data, meta = arff.loadarff(r"C:/sacanalysis/gazecom_small/all_features/beach/AAF_beach.arff")
+    df_original = pd.DataFrame(data)       
     df_original[df_original["confidence"]==0] = np.nan
     vel_df = GetVelocity(df_original,timestamp_col="time")
     vel_df = vel_df.dropna()
-    from pandas.testing import assert_series_equal
     original = vel_df["velocity"]
     mine = vel_df["velocity"]
     test2 = original!=mine