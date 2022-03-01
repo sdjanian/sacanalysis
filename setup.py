d9d8b1bfcec220afe523ed9013a4a3fe9e0bf440
Shagen Djanian <s.d@hotmail.dk>
2022-03-01 23:17:44 +0100

    Update setup.py
    updated setup with new github repo name

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     author="Shagen Djanian",
-    author_email="s.d@hotmail.dk",
+    author_email="sdjanian@cs.aau.dk",
     name='sacanalysis',
     license="MIT",
     description='sacanalysis is a package to analyse the quality of saccades and find potentially incorrect saccades.',
     version='v0.0.3',
-    url='https://github.com/Kongskrald/sacanalysis',
+    url='https://github.com/sdjanian/sacanalysis',
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=['matplotlib','numpy>=1.15','tqdm','scipy','seaborn','pandas>=0.23.4',
